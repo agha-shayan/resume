56d50007d02722cc4a405b3ce6fb80dcfcf15627
agha-shayan <shayan.askari12345@gmail.com>
2023-11-03 08:53:21 -0700

    added new things
    like the language i speack
    and using render to send data

--- app1/urls.py
+++ app1/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 from app1.views import *
 urlpatterns = [
     path('',index_view),
-    path("inner-page.html",inner_page)
+    #path("inner-page.html",inner_page)
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request,'index.html')
-def inner_page(request):
-    return render(request,"inner-page.html")
+    context={
+        "first_name":"mohammad",
+        "last_name":"askari",
+        "age":"20",
+        "email":"shayan.askari12345@gmail.com"
+            }
+    return render(request,'index.html',context)
+#def inner_page(request):
+#    return render(request,"inner-page.html")
